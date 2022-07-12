6dd55c4636a22ae140c14804b7f2ff3910772f94
SanjaySivappan205 <100414901+SanjaySivappan205@users.noreply.github.com>
2022-07-12 11:58:10 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Cpu_bound_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartBackgroundWrite(213434);
+        }
+      
+        
+        private static  async void StartBackgroundWrite(float value)
+        {
+            Console.WriteLine("Started CPU Bound asynchronous task on a background thread");
+            //Cpu bound operation is called here;
+            await Task.Run(() => SavingText(value));
+        }
+
+        //This method runs in the background;
+        private static  async void SavingText(float value)
+        {
+            //Finds the location path
+            string path = @"D:\Cpu.txt";
+            try
+            {
+                if (!File.Exists(path))
+                {
+                    using (var myFile = File.Create(path))
+                    {
+                        //Writes the file:
+                        TextWriter tw = new StreamWriter(path, false);
+                        tw.WriteLine(value);
+                        tw.Close();
+
+                    }
+
+                }
+                else if (File.Exists(path))
+                {
+                    using (var myFile = File.Create(path))
+                    {
+                        //writes the file:
+                        TextWriter tw = new StreamWriter(path, true);
+                        tw.WriteLine(value);
+                        tw.Close();
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        
+    }
+
+   
+}
